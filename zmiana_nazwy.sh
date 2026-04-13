e788f86f14ccc2dbd189ca436fe24de3492849a6
Jerz <124191947+JerzT@users.noreply.github.com>
2026-04-13 10:53:27 +0200

    Started working on changeing names
    added verb-verbouse option
    added force option
    changed code structure to be more precise
    added change_name_of_files function

--- zmiana_nazwy.sh
+++ zmiana_nazwy.sh
@@ -4,39 +4,57 @@
 listOfFiles=()
 listOfDirectories=()
 directory="$(pwd)"
-recursive=0
-verbouse=0
+recursive=false
+verbouse=false
+verbVerbouse=false
+force=false
 
 cleanup() {
-    printf "\nStopping...\n"
+    printf "Stopping...\n"
     kill "$spinner_pid" 2>/dev/null
     exit 1
 }
 
-
 look_for_directories_and_files () {
     local current_dir="$1"
+    local current_files=()
     local dirs=()
 
     for a in "$current_dir"/*; do
         [ -e "$a" ] || continue
 
         if [ -d "$a" ]; then
-            if [ "$recursive" -eq 1 ]; then
+            if [ $recursive = true ]; then
                 dirs+=("$a")
                 listOfDirectories+=("$a")
             fi
+
         else
+            current_files+=("$a")
             listOfFiles+=("$a")
         fi
     done
 
-    if  [ "$verbouse" -eq 1 ]; then
-        printf "Checked directory: %s\n" "$current_dir"
+    if  [ $verbouse = true -o $verbVerbouse = true ]; then
+        printf "\nChecked directory: %s\n" "$current_dir\n"
+        printf "Files found: ${#current_files[@]}\n"
+        printf "Directories found: ${#dirs[@]}\n"
+    fi
+
+    if [ $verbVerbouse = true ]; then
+        for b in "${current_files[@]}"; do
+            printf "File $b found in directory: $current_dir\n"
+        done
     fi
 
     for d in "${dirs[@]}"; do
         look_for_directories_and_files "$d"
+    done
+}
+
+change_name_of_files () {
+    for a in "${listOfFiles[@]}";do
+        echo $(stat -c %n $a)
     done
 }
 
@@ -56,10 +74,14 @@ while [[ "$#" -gt 0 ]]; do
     case $1 in
         -h|--help)
             echo "Usage: $0 [-h] [-r] [-d directory]"
-            echo "  -h, --help        Show this help"
-            echo "  -d, --directory   Set directory"
-            echo "  -r, --recursive   Enable recursive search"
-            echo "  -v, --verbouse    Print more info throught process"
+            echo "  -h, --help            Show this help"
+            echo "  -d, --directory       Set directory"
+            echo "  -r, --recursive       Enable recursive search"
+            echo "  -v, --verbouse        Print more info throught process"
+            echo "  -vv --verb-verbouse   Print all processes"
+            echo "  -f, --force           Force every changes for files"
+            echo "  -t, --tag             Use one of the tags to choose some type of a file"
+            echo "  -p, --pattern         Specify pattern of chnaged name"
             exit 0
             ;;
         -d|--directory)
@@ -67,10 +89,13 @@ while [[ "$#" -gt 0 ]]; do
             shift
             ;;
         -r|--recursive)
-            recursive=1
+            recursive=true
             ;;
         -v|--verbouse)
-            verbouse=1
+            verbouse=true
+            ;;
+        -vv|--verb-verbouse)
+            verbVerbouse=true
             ;;
         *)
             echo "Unknown option: $1"
@@ -82,7 +107,6 @@ done
 
 # run scan in background
 trap cleanup SIGINT
-
 # start spinner
 if [ "$verbouse" -eq 0 ]; then
     spin &
@@ -91,12 +115,17 @@ fi
 
 look_for_directories_and_files "$directory"
 
-
-
 # stop spinner
 kill "$spinner_pid" 2>/dev/null
 
 # clear line and finish
-printf "\rDone!                      \n"
+printf "\n\rDone!                      \n"
 printf "Files found: ${#listOfFiles[@]}\n"
 printf "Directories found: ${#listOfDirectories[@]}\n"
+
+if [ $force = false ]; then
+    read -p "Proceed to changing names? (y/n): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || cleanup
+fi
+
+change_name_of_files
+
