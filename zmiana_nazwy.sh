4dbc861abc26b6433eda2aa39bd1e23e97ef065e
Jerz <124191947+JerzT@users.noreply.github.com>
2026-04-15 00:18:14 +0200

    added tags
    added tags
    changed structure of file
    fixed typing mistake

--- zmiana_nazwy.sh
+++ zmiana_nazwy.sh
@@ -42,25 +42,33 @@ look_for_directories_and_files () {
 
             ext="${a##*.}"
 
-            for e in "${extensions[@]}"; do
+            if [[ ${#extensions[@]} = 0 ]];then
+                current_files+=("$a")
+                listOfFiles+=("$a")
+                continue
+            fi
+
+            for e in ${extensions[@]}; do
                 if [[ "$ext" == "$e" ]]; then
                     current_files+=("$a")
                     listOfFiles+=("$a")
                     break
                 fi
             done
+
         fi
     done
 
-    if  [ $verbouse = true -o $verbVerbouse = true ]; then
+    if  [ $verbose = true -o $very_verbose = true ]; then
         printf "\nChecked directory: %s\n" "$current_dir\n"
         printf "Files found: ${#current_files[@]}\n"
         printf "Directories found: ${#dirs[@]}\n"
     fi
 
-    if [ $verbVerbouse = true ]; then
+    if [ $very_verbose = true ]; then
         for b in "${current_files[@]}"; do
-            printf "File $b found in directory: $current_dir\n"
+            #printf "File $b found in directory: $current_dir\n"
+            printf "File $b found\n"
         done
     fi
 
@@ -85,21 +93,51 @@ spin() {
     done
 }
 
+set_extension_by_tag() {
+    case "$tag" in
+        music)
+            extensions=("mp3" "flac" "wav" "aac" "ogg")
+            ;;
+        video)
+            extensions=("mp4" "mkv" "avi" "mov" "webm")
+            ;;
+        images)
+            extensions=("jpg" "jpeg" "png" "gif" "bmp" "webp")
+            ;;
+    esac
+}
+
+#---------------------------------------------------------------------------------------------#
+# prepare program
+SCRIPT_PATH="$(realpath "$0")"
+listOfFiles=()
+listOfDirectories=()
+directory="$(pwd)"
+recursive=false
+verbose=false
+very_verbose=false
+force=false
+extensions=()
+tag=""
+ALLOWED_TAGS=("music" "video" "images")
+
 #---------------------------------------------------------------------------------------------#
 # argument parsing
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -h|--help)
-            echo "Usage: $0 [-h] [-r] [-d directory]"
-            echo "  -h, --help            Show this help"
-            echo "  -d, --directory       Set directory"
-            echo "  -r, --recursive       Enable recursive search"
-            echo "  -v, --verbouse        Print more info throught process"
-            echo "  -vv --verb-verbouse   Print all processes"
-            echo "  -f, --force           Force every changes for files"
-            echo "  -e, --extensions      Specify extensions of looked files"
-            echo "  -p, --pattern         Specify pattern of chnaged name"
-            echo "  -t, --tag             Use one of the tags to choose some type of a file"
+            echo "Usage: $0 [options]"
+            echo
+            echo "Options:"
+            echo "  -h, --help              Show this help message"
+            echo "  -d, --directory DIR     Set target directory"
+            echo "  -r, --recursive         Enable recursive search"
+            echo "  -v, --verbose           Print more info during process"
+            echo "  -vv, --very-verbose     Print detailed process info"
+            echo "  -f, --force             Force changes for files"
+            echo "  -e, --extensions        List of file extensions (space-separated)"
+            echo "  -p, --pattern PATTERN   Specify rename pattern"
+            echo "  -t, --tag TAG           Use predefined tag: music, video, images (by using this option you disable extensions given by you)"
             exit 0
             ;;
         -d|--directory)
@@ -110,16 +148,32 @@ while [[ "$#" -gt 0 ]]; do
             recursive=true
             ;;
         -v|--verbouse)
-            verbouse=true
+            verbose=true
             ;;
         -vv|--verb-verbouse)
-            verbVerbouse=true
+            very_verbose=true
             ;;
         -e|--extensions)
             shift
-            while [[ $# -gt 0 && "$1" != -* ]]; do
-                extensions+="$1"
+            while [[ $# -gt 0 && "$1" != "-"* ]]; do
+                extensions+=("$1")
+                shift
             done
+            continue
+            ;;
+        -p|--pattern)
+            shift
+            continue
+            ;;
+        -t|--tag)
+            if [[ " ${ALLOWED_TAGS[@]} " =~ " $2 " ]]; then
+                tag="$2"
+                set_extension_by_tag
+            else
+                echo "Error: Invalid tag '$2'. Allowed values are: ${ALLOWED_TAGS[*]}"
+                exit 1
+            fi
+            shift
             ;;
         *)
             echo "Unknown option: $1"
@@ -147,17 +201,6 @@ fi
 
 
 #---------------------------------------------------------------------------------------------#
-# prepare program
-SCRIPT_PATH="$(realpath "$0")"
-listOfFiles=()
-listOfDirectories=()
-directory="$(pwd)"
-recursive=false
-verbouse=false
-verbVerbouse=false
-force=false
-extensions=()
-
 # run scan in background
 trap cleanup SIGINT
 
